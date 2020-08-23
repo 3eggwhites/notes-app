022b43c64fdef37a3f30daa3435abf1b8f51f1eb
Ayan Pal <contact@ayanpal.co.in>
2020-08-23 21:51:11 +0530

    added initial codes for adding a note

--- notes.js
+++ notes.js
@@ -1,5 +1,34 @@
+const fs = require('fs');
+
+//to get notes
 const getNotes = () => {
     return 'Your Notes...';
+}
+
+const addNotes = (title, body) => {
+    const notes = loadNotes();
+    notes.push({
+        title: title,
+        body: body
+    });
+    console.log('New note added');
+}
+
+const filterNotesOnTitle = (notes, title) => {
+    notes.filter((note) => {
+        return title === note.title;
+    });
+    return filteredNotes;
+}
+
+const loadNotes = () => {
+    try{
+        const dataBuffer = fs.readFileSync('notes.json'); // data store for all the notes
+        const notesStringData = dataBuffer.toString();
+        return JSON.parse(notesStringData);
+    } catch (err) {
+        return [];
+    }
 }
 
 module.exports = getNotes;
