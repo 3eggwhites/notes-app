2887e4bbf0a180f6b4bf461b98081afd92c0e8d0
Ayan Pal <contact@ayanpal.co.in>
2020-08-24 01:05:15 +0530

    implemented remove fucntion

--- notes.js
+++ notes.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+const chalk = require('chalk');
+
 //to get notes
 const getNotes = () => {
     return 'Your Notes...';
@@ -7,28 +9,32 @@ const getNotes = () => {
 
 const addNotes = (title, body) => {
     const notes = loadNotes();
-    const duplicateNotes = filterNotesOnTitle(notes,title);
-    if (duplicateNotes.length === 0) {
+    const filteredNotes = notes.filter((note) => {
+        return title === note.title;
+    });
+    if (filteredNotes.length === 0) {
         notes.push({
             title: title,
             body: body
         });
         saveNotes(notes);
-        console.log('New note added');
+        console.log(chalk.green('New note added'));
     } else {
-        console.log('Title already taken.')
+        console.log(chalk.red('Title already taken.'));
     }
 }
 
 const removeNotes = (title) => {
-    console.log('Removed note' + title);
-}
-
-const filterNotesOnTitle = (notes, title) => {
-    const filteredNotes = notes.filter((note) => {
-        return title === note.title;
+    const notes = loadNotes();
+    const filteredOutNotes = notes.filter((note) => {
+        return title !== note.title;
     });
-    return filteredNotes;
+    if (notes.length > filteredOutNotes.length) {
+        saveNotes(filteredOutNotes);
+        console.log(chalk.green('Note with title '+title+' removed'));
+    } else {
+        console.log(chalk.red('No note found with title '+title+' to remove'));
+    }
 }
 
 const saveNotes = (notes) => {
